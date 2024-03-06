115dfe4cf0ff63e94d245e1ffc1445817e1209b0
MrYious <rosariomark37@gmail.com>
2024-03-06 22:03:53 +0800

    wrap root component in redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import './index.scss'
 
+import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.tsx'
+import { store } from './store.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store} >
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )

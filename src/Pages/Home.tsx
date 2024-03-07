b961c34e11108417db36c5921b3647036bfac081
MrYious <rosariomark37@gmail.com>
2024-03-07 12:32:42 +0800

    add hero section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,26 @@
+import './home.scss'
+
+import { FaAnglesDown } from "react-icons/fa6";
+import { useAppSelector } from '../hooks/useReduxHooks'
 
 export default function Home() {
+  const {theme} = useAppSelector(state => state.theme)
+
   return (
-    <div>Home1</div>
+    <>
+      <main className={theme}>
+        <h1>Dynamic Styles for Components</h1>
+        <p>Welcome to our dynamic styles for customizing components! We showcase beautiful and functional components crafted for a website. Take a look at our collection below</p>
+        <a href='#collection'>
+          <FaAnglesDown />
+        </a>
+      </main>
+      <section>
+        <aside>
+          
+        </aside>
+        We
+      </section>
+    </>
   )
 }

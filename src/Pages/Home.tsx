becebe6eef55f1d9991319604f48127affb2725c
MrYious <rosariomark37@gmail.com>
2024-03-07 22:03:07 +0800

    add sidenav and add template component

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,26 +1,54 @@
 import './home.scss'
 
+import ComponentTemplate from '../components/ComponentTemplate';
 import { FaAnglesDown } from "react-icons/fa6";
 import { useAppSelector } from '../hooks/useReduxHooks'
 
 export default function Home() {
   const {theme} = useAppSelector(state => state.theme)
 
+  const list = [
+    {id: '#input', name: 'Inputs'},
+    {id: '#button', name: 'Buttons'},
+    {id: '#alert', name: 'Alerts'},
+    {id: '#card', name: 'Cards'},
+    {id: '#navigation', name: 'Navigation'},
+    {id: '#form', name: 'Forms'},
+    {id: '#modal', name: 'Models'},
+    {id: '#tab', name: 'Tabs'},
+    {id: '#accordion', name: 'Accordion'},
+    {id: '#table', name: 'Tables'},
+    {id: '#date_time', name: 'Date and Time Picker'},
+    {id: '#menu', name: 'Dropdown'},
+    {id: '#tag_badge', name: 'Tags and Badges '},
+    {id: '#footer', name: 'Footer'},
+    {id: '#progress', name: 'Progress Bar'},
+  ]
+
   return (
     <>
       <main className={theme}>
-        <h1>Dynamic Styles for Components</h1>
-        <p>Welcome to our dynamic styles for customizing components! We showcase beautiful and functional components crafted for a website. Take a look at our collection below</p>
+        <h1>Customized Styles for Components</h1>
+        <p>Welcome to our showcase of various components with different customization! We showcase beautiful and functional components crafted for websites. Take a look at our collection below</p>
         <a href='#collection'>
           <FaAnglesDown />
         </a>
       </main>
-      <section>
+      <div id='collection' className={theme}>
         <aside>
-          
+          <ul>
+            {
+              list.map(link =>
+                <li key={link.id}><a href={link.id}>{link.name}</a></li>
+              )
+            }
+          </ul>
         </aside>
-        We
-      </section>
+        <div>
+          <ComponentTemplate />
+          <ComponentTemplate />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/components/ComponentTemplate.tsx
@@ -0,0 +1,14 @@
+import './_content.scss'
+
+export default function ComponentTemplate() {
+
+    return (
+        <section id="sampleID">
+            <h2>This is a sample Title</h2>
+            <p>
+                
+            </p>
+            <div></div>
+        </section>
+    )
+}

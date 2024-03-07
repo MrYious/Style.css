15f17b7e2d95e7b66203c3173701ebf61dcfdd06
MrYious <rosariomark37@gmail.com>
2024-03-07 22:34:58 +0800

    update template

--- src/components/ComponentTemplate.tsx
+++ src/components/ComponentTemplate.tsx
@@ -1,13 +1,21 @@
-import './_content.scss'
+import { useAppSelector } from "../hooks/useReduxHooks"
 
 export default function ComponentTemplate() {
 
+    const { theme } = useAppSelector(state => state.theme)
+
     return (
-        <section id="sampleID">
+        <section id="sampleID" className={theme}>
             <h2>This is a sample Title</h2>
             <p>
-                
+                Sample Description
             </p>
+            <p>
+                Sample Description
+            </p>
+            <div className="preview">
+                sample
+            </div>
             <div></div>
         </section>
     )

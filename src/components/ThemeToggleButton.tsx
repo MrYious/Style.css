665aa258def842a7171a6ea8fc0df4cc452c8af1
MrYious <rosariomark37@gmail.com>
2024-03-07 09:47:51 +0800

    style theme toggle button

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,15 +1,28 @@
 import './themetogglebutton.scss'
 
 import { FaMoon, FaSun } from "react-icons/fa";
+import { useAppDispatch, useAppSelector } from '../hooks/useReduxHooks';
+
+import { toggleTheme } from '../slicers/ThemeSlicer';
 
 export default function ThemeToggleButton() {
 
-    return(<div className="toggleButton light">
-        <button>
-            <FaMoon />
-        </button>
-        <button>
-            <FaSun />
-        </button>
-    </div>)
+    const { theme } = useAppSelector(state => state.theme)
+    const dispatch = useAppDispatch()
+
+    const handleToggleTheme = () => {
+        dispatch(toggleTheme())
+    }
+
+    return(<button className={`toggleButton ${theme}`} onClick={handleToggleTheme}>
+        <span>
+            {
+                theme === 'light' ? (
+                    <FaSun id='light'/>
+                ) : (
+                    <FaMoon id='dark'/>
+                )
+            }
+        </span>
+    </button>)
 }

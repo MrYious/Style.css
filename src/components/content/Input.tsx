368c71b1328cd96abb9f0f4c4683591a3fc0510d
MrYious <rosariomark37@gmail.com>
2024-03-08 22:15:16 +0800

    complete input content

--- src/components/content/Input.tsx
+++ src/components/content/Input.tsx
@@ -1,26 +1,46 @@
-import './component.scss';
+import './input.scss';
 
-import { useAppSelector } from "../../hooks/useReduxHooks"
+import { useAppDispatch, useAppSelector } from "../../hooks/useReduxHooks"
+
+import { setName } from '../../slicers/ContentSlicer';
 
 export default function Input() {
 
     const { theme } = useAppSelector(state => state.theme)
+    const { name } = useAppSelector(state => state.content)
+    const dispatch = useAppDispatch()
+
+    const handleUpdateName = (e: any) => {
+        dispatch(setName(e.target.value))
+    }
 
     return (
         <section id="input" className={theme}>
             <h2>Input Elements</h2>
             <h3>
+                Text
+            </h3>
+            <div className="preview">
+                <input type="text" name="name" value={name} onChange={handleUpdateName} placeholder='Name'/>
+                <div id='group1'>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={name} onChange={handleUpdateName}/>
+                </div>
+                <div id='group2'>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={name} onChange={handleUpdateName}/>
+                </div>
+                <div id='group3'>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={name} onChange={handleUpdateName}/>
+                </div>
+            </div>
+            {/* <h3>
                 Sample Description
             </h3>
             <div className="preview">
                 sample
-            </div>
-            <p>
-                Sample Description
-            </p>
-            <div className="preview">
-                sample
-            </div>
+            </div> */}
         </section>
     )
 }
